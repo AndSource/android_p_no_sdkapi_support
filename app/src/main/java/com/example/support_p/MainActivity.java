141c90bd83bac18b4107bc703cd10a55bc527d5f
sanbo <xueyongfu@analysys.com.cn>
2018-09-20 14:03:09 +0800

    update

--- app/src/main/java/com/example/support_p/MainActivity.java
+++ app/src/main/java/com/example/support_p/MainActivity.java
@@ -1,92 +1,97 @@
 package com.example.support_p;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
-  // Used to load the 'native-lib' library on application startup.
-  static {
-    System.loadLibrary("substrate");
-    System.loadLibrary("test2");
-    System.loadLibrary("native-lib");
-  }
+    // Used to load the 'native-lib' library on application startup.
+//    static {
+//        System.loadLibrary("substrate");
+//        System.loadLibrary("test2");
+//        System.loadLibrary("native-lib");
+//    }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
 
-    ReflectionHelper.testMethod();
-    ReflectionHelper.testField();
-    findViewById(R.id.test_method).setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        try {
-          Class clz = Class.forName("android.app.ActivityThread");
-          Method method = clz.getDeclaredMethod("getIntentBeingBroadcast");
-          method.setAccessible(true);
-          method.invoke(null);
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-      }
-    });
-
-    findViewById(R.id.test_field).setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        try {
-          Class clz = Class.forName("android.app.ActivityThread");
-          Field field = clz.getDeclaredField("TAG");
-          field.setAccessible(true);
-          field.get(null);
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-      }
-    });
-
-    findViewById(R.id.reflection_helper).setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        testJavaPojie();
-      }
-    });
-
-    findViewById(R.id.test_reflection_helper).setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
         ReflectionHelper.testMethod();
         ReflectionHelper.testField();
-      }
-    });
-  }
+        findViewById(R.id.test_method).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Class clz = Class.forName("android.app.ActivityThread");
+                    Method method = clz.getDeclaredMethod("getIntentBeingBroadcast");
+                    method.setAccessible(true);
+                    method.invoke(null);
+                    Log.i("xxx","test_method----success");
+                } catch (Throwable e) {
+                    Log.e("xxx","test_method----failed\n" + Log.getStackTraceString(e));
+                }
+            }
+        });
 
-  private void testJavaPojie() {
-    try {
-      Class reflectionHelperClz = Class.forName("com.example.support_p.ReflectionHelper");
-      Class classClz = Class.class;
-      Field classLoaderField = classClz.getDeclaredField("classLoader");
-      classLoaderField.setAccessible(true);
-      classLoaderField.set(reflectionHelperClz, null);
-    } catch (Exception e) {
-      e.printStackTrace();
+        findViewById(R.id.test_field).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Class clz = Class.forName("android.app.ActivityThread");
+                    Field field = clz.getDeclaredField("TAG");
+                    field.setAccessible(true);
+                    field.get(null);
+                    Log.i("xxx","test_field----success");
+                } catch (Throwable e) {
+                    Log.e("xxx","test_field----failed\n" + Log.getStackTraceString(e));
+                }
+            }
+        });
+
+        findViewById(R.id.reflection_helper).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testJavaPojie();
+            }
+        });
+
+        findViewById(R.id.test_reflection_helper).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ReflectionHelper.testMethod();
+                ReflectionHelper.testField();
+            }
+        });
     }
-  }
 
-  /**
-   * A native method that is implemented by the 'native-lib' native library,
-   * which is packaged with this application.
-   */
-  public native String stringFromJNI();
+    private void testJavaPojie() {
+        try {
+            Class reflectionHelperClz = Class.forName("com.example.support_p.ReflectionHelper");
+            Class classClz = Class.class;
+            Field classLoaderField = classClz.getDeclaredField("classLoader");
+            classLoaderField.setAccessible(true);
+            classLoaderField.set(reflectionHelperClz, null);
+            Log.i("xxx","testJavaPojie----success");
+        } catch (Throwable e) {
+            Log.e("xxx","testJavaPojie----failed\n" + Log.getStackTraceString(e));
+        }
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+//    public native String stringFromJNI();
 }
--- app/src/main/java/com/example/support_p/ReflectionHelper.java
+++ app/src/main/java/com/example/support_p/ReflectionHelper.java
@@ -1,37 +1,37 @@
 package com.example.support_p;
 
 import android.util.Log;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class ReflectionHelper {
 
-  private static final String TAG = ReflectionHelper.class.getSimpleName();
+    private static final String TAG = ReflectionHelper.class.getSimpleName();
 
-  public static void testMethod() {
-    try {
-      Class clz = Class.forName("android.app.ActivityThread");
-      Method method = clz.getDeclaredMethod("getIntentBeingBroadcast");
-      method.setAccessible(true);
-      method.invoke(null);
-      Log.e(TAG, "testMethod: 反射成功");
-    } catch (Exception e) {
-      e.printStackTrace();
-      Log.e(TAG, "testMethod: 反射失败");
-    }
-  }
+    public static void testMethod() {
+        try {
+            Class clz = Class.forName("android.app.ActivityThread");
+            Method method = clz.getDeclaredMethod("getIntentBeingBroadcast");
+            method.setAccessible(true);
+            method.invoke(null);
 
-  public static void testField() {
-    try {
-      Class clz = Class.forName("android.app.ActivityThread");
-      Field field = clz.getDeclaredField("TAG");
-      field.setAccessible(true);
-      field.get(null);
-      Log.e(TAG, "testField: 反射成功");
-    } catch (Exception e) {
-      e.printStackTrace();
-      Log.e(TAG, "testField: 反射失败");
+            Log.i("xxx", "ReflectionHelper.testMethod----success");
+        } catch (Throwable e) {
+            Log.e("xxx", "ReflectionHelper.testMethod----failed\n" + Log.getStackTraceString(e));
+        }
     }
 
-  }
+    public static void testField() {
+        try {
+            Class clz = Class.forName("android.app.ActivityThread");
+            Field field = clz.getDeclaredField("TAG");
+            field.setAccessible(true);
+            field.get(null);
+            Log.i("xxx", "ReflectionHelper.testField----success");
+        } catch (Throwable e) {
+            Log.e("xxx", "ReflectionHelper.testField----failed\n" + Log.getStackTraceString(e));
+        }
+
+    }
 }
